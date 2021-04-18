c8011fe426003e5ee8cb1110eb3620ff9c26f16d
Pouya Zahedi <p.zahedi@live.com>
2021-04-19 01:54:23 +0430

    interpolate with uniform steps, save output function added

--- fixLTSpicedata.py
+++ fixLTSpicedata.py
@@ -1,64 +1,55 @@
-# Laus deo
-# fix LTSpice data
-# LTSpice is a free opensource tool for simulating digital and analog circuit.
-# The point is after save the result of simulation, the software automatically choose the sampleling frequency and it's not a constant.
-# During my search it's an unsolved problem and I tried to fix it. 
-# CAUTION: it's work for only 1 input and 1 output signals, but it's open source and you (as a developer) can modify it as you need. 
-# Developer: P.Zahedi
-# E-mail: p.zahedi@live.com
+'''
+Laus deo
+fix LTSpice data
+LTSpice is a free opensource tool for simulating digital and analog circuit.
+The point is after save the result of simulation, the software automatically choose the sampleling frequency and it's not a constant.
+During my search it's an unsolved problem and I tried to fix it. 
+
+***********************
+in data file :  
+          Time: SampleData[:,0] 1st column
+          Data: SampleData[:,1] 2nd column
+        Target: SampleData[:,2] 3rd column 
+
+CAUTION: it's work for only 1 input and 1 output signals, but it's open source and you (as a developer) can modify it as you need.
+but this problem will be fixed soon and work for dynamicly range of data
+
+***********************
+Developer: P.Zahedi
+E-mail: p.zahedi@live.com
+'''
 
 import numpy as np
+from scipy.interpolate import interp1d
 
+     # interpolate 
+     
+def UniformSteps(DataFileAddress,TimeSteps=200,kind='linear'):
+    # read data file
+    '''
+    DataFileAddress : address of .txt data file.
+    TimeSteps: number of output steps, default 200 row of data will generate.
+    kind: interpolation kind. defulat is linear but it can be 'nearest' , 'cubic' or etc. (look at scypy documents).
+    '''    
+    SampleData=np.loadtxt(DataFileAddress,dtype=float)
+ 
+    #interpolate data
+    input_interpolate=interp1d(SampleData[:,0],SampleData[:,1],kind=kind)
+    output_interpolate=interp1d(SampleData[:,0],SampleData[:,2],kind=kind)
 
-# class FixLTSpiceData:
-
-    # change inter polate between points and fix the sampeling steps
-    # Liniear interpolation is used to interpolate between two points 
-def interpolator(y2,t2,y1,t1):
-    m=(y2-y1)/(t2-t1)
-    T_new=np.linspace(t1,t2,num=t2-t1,endpoint=True)
-    Y_new = m*(T_new - t1) +y1
-    return Y_new
-
-# Dwonsample data space into a smaller space. for ML tools, typically length of output signal is 300-500 points
-def downsampler(FixedVector,length):
-    Y=[0]*length # create a large vector to downsample the original space
-    m=int(len(FixedVector)/length) 
-    for i in range(length):
-        Y[i]=FixedVector[i*m]
-
-    return Y
-
-def fixLTSpiceData(FileAddress,FinalLength):
+    #find border of Time vector and generate new time vector with fixed steps
+    mintime=min(SampleData[:,0])
+    maxtime=max(SampleData[:,0])
+    newtime=np.linspace(mintime ,maxtime,num=TimeSteps,endpoint=False)
     
-    Time=[0]
-    Data=[0]
-    Target=[0]
-    Time_new=[]
-    Data_new=[]
-    Target_new=[]
+    return np.transpose([newtime,input_interpolate(newtime),output_interpolate(newtime)])
 
-    Result=np.loadtxt(FileAddress,dtype=float)
-    Time=np.reshape(Result[:,0],(len(Result),1))*10**+9
-    Data=np.reshape(Result[:,1],(len(Result),1))
-    Target=np.reshape(Result[:,3],(len(Result),1))
 
-    # interpolate(fix) vectors
-    for i in range(1,len(Time)):
-        Time_new=[*Time_new[0:len(Time_new)-1],*(interpolator(Time[i],Time[i],Time[i-1],Time[i-1]))]
-        Data_new=[*Data_new[0:len(Time_new)-1],*(interpolator(Data[i],Time[i],Data[i-1],Time[i-1]))]
-        Target_new=[*Target_new[0:len(Target_new)-1],*(interpolator(Target[i],Time[i],Target[i-1],Time[i-1]))]
-        print(str(100*i/len(Time))+"% passed interpolating")
-    print("100.00%  passed interpolating ")
-
-    # reshape and downsample fixed data
-    Time_new =np.reshape(downsampler(np.array(Time_new)*10**-9,FinalLength),(FinalLength, ))
-    print('Time vector downsampled')
-    Data_new =np.reshape(downsampler(np.array(Data_new),FinalLength),(FinalLength, ))
-    print('Data vector downsampled')
-    Target_new=np.reshape(downsampler(np.array(Target_new),FinalLength),(FinalLength, ))
-    print('Target vector downsampled')
-
-    # return fixed and downsampled space
-    FixedResult=np.transpose((Time_new,Data_new,Target_new))
-    return FixedResult
+def SavedUniformedData(UniformedFileAddress,UniformedData,delimiter='\t'):
+    ''' 
+    UniformedFileAddress: name and address of uniformed  data to save it. .txt format
+    UniformedData: array of data that has been uniformed in given steps.
+    delimiter: delimiter of output file
+    '''
+    np.savetxt(UniformedFileAddress,UniformedData ,delimiter=delimiter)
+    print('werite succesfull')
--- fixLTSpicedata_example.py
+++ fixLTSpicedata_example.py
@@ -1,14 +1,27 @@
-# Laus deo
-# fix LTSpice data
-# LTSpice is a free opensource tool for simulating digital and analog circuit.
-# The point is after save the result of simulation, the software automatically choose the sampleling frequency and it's not a constant.
-# During my search it's an unsolved problem and I tried to fix it. 
-# CAUTION: it's work for only 1 input and 1 output signals, but it's open source and you (as a developer) can modify it as you need. 
-# Developer: P.Zahedi
-# E-mail: p.zahedi@live.com
+'''
+Laus deo
+fix LTSpice data
+LTSpice is a free opensource tool for simulating digital and analog circuit.
+The point is after save the result of simulation, the software automatically choose the sampleling frequency and it's not a constant.
+During my search it's an unsolved problem and I tried to fix it. 
 
-# to use this softare type in terminal: 
-# python3.8 fixLTspice data.py <original file> <number of output point> 
+***********************
+in data file :  
+          Time: SampleData[:,0] 1st column
+          Data: SampleData[:,1] 2nd column
+        Target: SampleData[:,2] 3rd column 
+
+CAUTION: it's work for only 1 input and 1 output signals, but it's open source and you (as a developer) can modify it as you need.
+but this problem will be fixed soon and work for dynamicly range of data
+
+***********************
+Developer: P.Zahedi
+E-mail: p.zahedi@live.com
+
+***********************
+to use this softare type in terminal: 
+python3.8 fixLTspice data.py <original file> <number of output point> 
+'''
 
 import numpy as np
 import sys
@@ -21,7 +34,7 @@ if __name__ == "__main__":
     FixedFileAddress='Fixed/'+sys.argv[1]
 
     # fix (inter polate and downsample the dataset file)
-    FixedResult = LTfixer.fixLTSpiceData(FileAddress,DownsampleNum)
+    FixedResult = LTfixer.UniformSteps(FileAddress,DownsampleNum)
     
     # save fixed data
-    np.savetxt(FixedFileAddress,FixedResult,delimiter='\t')
+    LTfixer.sa
