8b7395a564369df6b1bd18579601948ac724cbce
Pouya Zahedi <p.zahedi@live.com>
2021-04-19 11:15:48 +0430

    multi column of data fixed, uniform meticulous data added to module again

--- fixLTSpicedata.py
+++ fixLTSpicedata.py
@@ -11,9 +11,6 @@ in data file :
           Data: SampleData[:,1] 2nd column
         Target: SampleData[:,2] 3rd column 
 
-CAUTION: it's work for only 1 input and 1 output signals, but it's open source and you (as a developer) can modify it as you need.
-but this problem will be fixed soon and work for dynamicly range of data
-
 ***********************
 Developer: P.Zahedi
 E-mail: p.zahedi@live.com
@@ -24,31 +21,85 @@ from scipy.interpolate import interp1d
 
 
 
-     # interpolate 
+    # change inter polate between points and uniform the sampeling steps
+    # Liniear interpolation is used to interpolate between two points 
+def interpolator(y2,t2,y1,t1):
+    m=(y2-y1)/(t2-t1)
+    T_new=np.linspace(t1,t2,num=t2-t1,endpoint=True)
+    Y_new = m*(T_new - t1) +y1
+    return Y_new
+
+# Dwonsample data space into a smaller space. for ML tools, typically length of output signal is 300-500 points
+def downsampler(FixedVector,length):
+    Y=[0]*length # create a large vector to downsample the original space
+    m=int(len(FixedVector)/length) 
+    for i in range(length):
+        Y[i]=FixedVector[i*m]
+
+    return Y
+
+    # uniform data more accurate 
+def UniformStepsMeticulous(SampleData,TimeSteps=200):
+    '''
+    This function will produce a very large vector of Data, has alot of calculation but time steps are equivalent
+    SampleData : array of none uniform distributed data of time, input and target.
+    TimeSteps: number of output steps, default 200 row of data will generate.    
+    '''
+    SampleData[:,0]=SampleData[:,0]*10**+9
+    UniformedData=[0]*np.shape(SampleData)[1]
+
+    # interpolate (uniform) vectors
+    for j in range(len(UniformedData)):
+        for i in range(1,len(SampleData[:,0])):
+            
+            # interpolate and uniform data 
+            UniformedData[j]=[*UniformedData[0:len(UniformedData)-1],*(interpolator(SampleData[:,j][i],SampleData[:,0][i],SampleData[:,j][i-1],SampleData[:,0][i-1]))]
+
+            #downsample the large vector with given time steps
+            if j==0:
+                UniformedData[j] =np.reshape(downsampler(np.array(UniformedData[j])*10**-9,TimeSteps),(TimeSteps, ))       
+            else:
+                UniformedData[j] =np.reshape(downsampler(np.array(UniformedData[j]),TimeSteps),(TimeSteps, ))       
+
+            print(str(100*i/len(SampleData[:,0]))+"% passed interpolating")
+    print("100.00%  passed interpolating ")
+
+    
+    # return fixed and downsampled space
+    return np.transpose(UniformedData)
+
+
+
+
+    # interpolate 
 def UniformSteps(SampleData,TimeSteps=200,kind='linear'):
     '''
     SampleData : array of none uniform distributed data of time, input and target.
     TimeSteps: number of output steps, default 200 row of data will generate.
     kind: interpolation kind. defulat is linear but it can be 'nearest' , 'cubic' or etc. (look at scypy documents).
     '''    
-     
-    #interpolate data
-    input_interpolate=interp1d(SampleData[:,0],SampleData[:,1],kind=kind)
-    output_interpolate=interp1d(SampleData[:,0],SampleData[:,2],kind=kind)
-
+    
+    UniformedData=[0]*np.shape(SampleData)[1]
+    
     #find border of Time vector and generate new time vector with fixed steps
     mintime=min(SampleData[:,0])
     maxtime=max(SampleData[:,0])
-    newtime=np.linspace(mintime ,maxtime,num=TimeSteps,endpoint=False)
-    
-    return np.transpose([newtime,input_interpolate(newtime),output_interpolate(newtime)])
+    UniformedData[0]=np.linspace(mintime ,maxtime,num=TimeSteps,endpoint=False) # new time steps
+
+    #interpolate data
+    for i in range(1,len(UniformedData)):
+        interpolatedfunction=interp1d(SampleData[:,0],SampleData[:,i],kind=kind)
+        UniformedData[i]=interpolatedfunction(UniformedData[0])
+
+      
+    return np.transpose(UniformedData)
 
 
-def SavedUniformedData(UniformedFileAddress,UniformedData,delimiter='\t'):
+def SaveUniformedData(FileSaveAddress,UniformedData,delimiter='\t'):
     ''' 
-    UniformedFileAddress: name and address of uniformed  data to save it. .txt format
+    FileSaveAddress: name and address of uniformed  data to save it with .txt format.
     UniformedData: array of data that has been uniformed in given steps.
     delimiter: delimiter of output file
     '''
-    np.savetxt(UniformedFileAddress,UniformedData ,delimiter=delimiter)
+    np.savetxt(FileSaveAddress,UniformedData ,delimiter=delimiter)
     print('werite succesfull')
